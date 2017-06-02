5ec4e632e9950b1540ac218600a568a3b609df09
kimi <kimi_tsai@tw.shu.comttle>
2017-06-02 13:32:24 +0800

    add more info

--- currency.js
+++ currency.js
@@ -1,5 +1,9 @@
-var USNOTIFY = 30;
+﻿var USNOTIFY = 30.08;
 var JPNOTIFY = 0.2650;
+var MIN_US = 65535;
+var MIN_JP = 65535;
+var MAX_US = 0;
+var MAX_JP = 0;
 var MAX_USJP = 0;
 var MIN_USJP = 65535;
 
@@ -38,6 +42,10 @@ function esunbank() {
 	c.queue([{
 		url: 'https://www.esunbank.com.tw/bank/personal/deposit/rate/forex/foreign-exchange-rates',
 		callback: function(error, result, $) {
+			let lastDay = $("input#TxtDatepicker").val().toString();
+			let lastTime = $("input#Txt_deposit1").val().toString();
+			let lastUpdate = lastDay +" "+ lastTime;
+			lastUpdate = moment(lastUpdate).format('MM/DD HH:mm');
 
 			let us = $("TD:contains('美元(USD)')");
 			let usDollar = {
@@ -60,9 +68,15 @@ function esunbank() {
 			let us_vs_jp = get_proportion(usDollar.SpotSelling, jpDollar.SpotSelling);
 			MAX_USJP = Math.round((Math.max(MAX_USJP, us_vs_jp) * 1000)) / 1000;
 			MIN_USJP = Math.round((Math.min(MIN_USJP, us_vs_jp) * 1000)) / 1000;
-			console.log("US/JP = %d \t MAX_USJP = %d \t MIN_USJP = %d \t", us_vs_jp, MAX_USJP, MIN_USJP);
+			console.log("US/JP = %d \t MAX_USJP = %d \t MIN_USJP = %d \t ", us_vs_jp, MAX_USJP, MIN_USJP);
+			
+			MIN_US = usDollar.SpotSelling <= MIN_US ? usDollar.SpotSelling : MIN_US;
+			MAX_US = usDollar.SpotSelling >= MAX_US ? usDollar.SpotSelling : MAX_US;
+			MIN_JP = jpDollar.SpotSelling <= MIN_JP ? jpDollar.SpotSelling : MIN_JP;
+			MAX_JP = jpDollar.SpotSelling >= MAX_JP ? jpDollar.SpotSelling : MAX_JP;
+			console.log("MIN_US = %d \t MAX_US = %d \t MIN_JP = %d \t MAX_JP = %d", MIN_US, MAX_US, MIN_JP, MAX_JP);
 
-			console.log("----------------------------------------------------------------------------------------------------");
+			console.log("--------------------------------------"+lastUpdate+"---------------------------------------------------");
 			print_col(usDollar);
 			console.log("----------------------------------------esunbank----------------------------------------------------");
 			print_currency(usDollar);
@@ -71,7 +85,7 @@ function esunbank() {
 			print_currency(jpDollar);
 			notify(jpDollar, JPNOTIFY);
 
-			console.log("\n");
+			console.log("\n\n");
 		}
 	}]);
 }
@@ -88,4 +102,4 @@ var c = new Crawler({
 });
 
 esunbank();
-setInterval(esunbank, 600000);
+setInterval(esunbank, 300000);
